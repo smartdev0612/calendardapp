12e13f8a13d4ffe073d28ba004cf8803ed52acec
Daniel Morales <danieldev0612@gmail.com>
2022-05-19 13:25:56 -0700

    first commit

---
+++ tests/calculatordapp.ts
@@ -0,0 +1,32 @@
+const assert = require('assert')
+const anchor = require('@project-serum/anchor')
+const {SystemProgram} = anchor.web3
+describe('calculatordapp', () => {
+    const provider = anchor.Provider.local()
+    anchor.setProvider(provider)
+    const calculator = anchor.web3.Keypair.generate()
+    const program = anchor.workspace.Calculatordapp
+
+    if ('Creates a calculator', async () => {
+        await program.rpc.create("Welcome to Solana", {
+            accounts: {
+                calculator: calculator.publicKey,
+                user: provider.wallet.publicKey,
+                systemProgram: SystemProgram.programId
+            },
+            signers: [calculator]
+        })
+        const account = await program.account.calculator.fetch(calculator.publicKey)
+        assert.ok(account.greeting === "Welcome to Solana")
+    })
+
+    it ('Adds two numbers', async () => {
+        await program.rpc.add(new anchor.BN(2), new anchor.BN(3), {
+            accounts: {
+                calculator: calculator.publicKey
+            }
+        })
+        const account = await program.account.calculator.fetch(calculator.publicKey)
+        assert.ok(account.result.eq(new anchor.BN(5)))
+    })
+})
